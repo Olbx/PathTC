03f54e37d7eab77126d54b584e0b8e16785d42bb
Olbx <akamtales@gmail.com>
2014-05-17 11:01:25 -0700

    Update SpellMgr.cpp

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3693,6 +3693,11 @@ void SpellMgr::LoadSpellInfoCorrections()
 
         switch (spellInfo->SpellFamilyName)
         {
+            case SPELLFAMILY_HUNTER:
+                // Silencing Shot / Scatter Shot
+                if (spellInfo->SpellFamilyFlags[0] & 0x40000)
+                    spellInfo->Speed = 0; // instant
+                break;
             case SPELLFAMILY_PALADIN:
                 // Seals of the Pure should affect Seal of Righteousness
                 if (spellInfo->SpellIconID == 25 && spellInfo->Attributes & SPELL_ATTR0_PASSIVE)
